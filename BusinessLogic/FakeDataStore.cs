af6d5f35b9fc368a451191b9bf9560e64481de04
Ihor Mizyk <ihm@interlogic.com.ua>
2023-11-02 00:45:13 +0200

    added fake data store

---
+++ BusinessLogic/FakeDataStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BusinessLogic.Models;
+
+namespace BusinessLogic
+{
+    public class FakeDataStore
+    {
+        private static List<Employee> employees;
+
+        public FakeDataStore()
+        {
+            employees = new List<Employee>
+            {
+                new() { Id = 1, Name = "Andruha" },
+                new() { Id = 2, Name = "Ksuha" },
+                new() { Id = 3, Name = "Dimon" }
+            };
+        }
+
+        public async Task<Employee> AddEmployee(string name)
+        {
+            employees.Add(new Employee { Id = employees.Last().Id + 1, Name = name });
+
+            return await Task.FromResult(employees.Where(e => e.Name == name).First());
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployees() => await Task.FromResult(employees);
+    }
+}
