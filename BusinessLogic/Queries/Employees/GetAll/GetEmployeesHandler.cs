a4fa4eedd98dc87923bf79770ced610250bc322a
Ihor Mizyk <ihm@interlogic.com.ua>
2023-11-02 00:43:18 +0200

    added get employees command and handler

---
+++ BusinessLogic/Queries/Employees/GetAll/GetEmployeesHandler.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogic.Models;
+
+using MediatR;
+
+namespace BusinessLogic.Queries.Employees.GetAll
+{
+    public class GetEmployeesHandler : IRequestHandler<GetEmployeesQuery, IEnumerable<Employee>>
+    {
+        private readonly FakeDataStore _fakeDataStore;
+
+        public GetEmployeesHandler(FakeDataStore fakeDataStore) 
+        {
+            _fakeDataStore = fakeDataStore;
+        }
+
+        public Task<IEnumerable<Employee>> Handle(GetEmployeesQuery query, CancellationToken cancellationToken)
+        {
+            var employees = _fakeDataStore.GetAllEmployees();
+
+            return employees;
+        }
+    }
+}
---
+++ BusinessLogic/Queries/Employees/GetAll/GetEmployeesQuery.cs
@@ -0,0 +1,8 @@
+﻿using BusinessLogic.Models;
+
+using MediatR;
+
+namespace BusinessLogic.Queries.Employees.GetAll
+{
+    public record GetEmployeesQuery(): IRequest<IEnumerable<Employee>>;
+}
