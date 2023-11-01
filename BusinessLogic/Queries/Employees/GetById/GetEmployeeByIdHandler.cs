23b3500e145d94847412984b88a382d1f59c24bd
Ihor Mizyk <ihm@interlogic.com.ua>
2023-11-02 00:43:30 +0200

    added get employee by id command and handler

---
+++ BusinessLogic/Queries/Employees/GetById/GetEmployeeByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLogic.Models;
+
+using MediatR;
+
+namespace BusinessLogic.Queries.Employees.GetById
+{
+    public class GetEmployeeByIdHandler : IRequestHandler<GetEmployeeByIdQuery, Employee>
+    {
+        private readonly FakeDataStore _fakeDataStore;
+
+        public GetEmployeeByIdHandler(FakeDataStore fakeDataStore)
+        {
+            _fakeDataStore = fakeDataStore;
+        }
+
+        public async Task<Employee> Handle(GetEmployeeByIdQuery request, CancellationToken cancellationToken)
+        {
+            var employee = (await _fakeDataStore.GetAllEmployees()).FirstOrDefault(e => e.Id == request.Id);
+
+            return employee;
+        }
+    }
+}
---
+++ BusinessLogic/Queries/Employees/GetById/GetEmployeeByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using BusinessLogic.Models;
+
+using MediatR;
+
+namespace BusinessLogic.Queries.Employees.GetById
+{
+    public record GetEmployeeByIdQuery(int Id) : IRequest<Employee>;
+}
