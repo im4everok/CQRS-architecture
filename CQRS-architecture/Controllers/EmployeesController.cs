c15dcc566584b4ae9a1bb359376d9c6e185ab728
Ihor Mizyk <imizyk09@gmail.com>
2024-02-23 17:12:58 +0200

    added IPublisher from mediatR and publish employee created notification

--- CQRS-architecture/Controllers/EmployeesController.cs
+++ CQRS-architecture/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLogic.Commands.Employees.CreateEmployee;
+using BusinessLogic.Notifications.Employees;
 using BusinessLogic.Queries.Employees.GetAll;
 using BusinessLogic.Queries.Employees.GetById;
 using BusinessLogic.Queries.Employees.GetCachedEmployeeByIdQuery;
@@ -15,10 +16,12 @@ namespace CQRS_architecture.Controllers
     public class EmployeesController : ControllerBase
     {
         private readonly ISender _mediator;
+        private readonly IPublisher _mediatorPublisher;
 
-        public EmployeesController(ISender mediator)
+        public EmployeesController(ISender mediator, IPublisher mediatorPublisher)
         {
             _mediator = mediator;
+            _mediatorPublisher = mediatorPublisher;
         }
 
         [HttpGet]
@@ -33,7 +36,9 @@ namespace CQRS_architecture.Controllers
         public async Task<IActionResult> TestPost([FromBody] string name)
         {
             var addedEmployee = await _mediator.Send(new CreateEmployeeCommand(new() { Name = name }));
-            
+
+            await _mediatorPublisher.Publish(new EmployeeCreatedNotification(addedEmployee));
+
             return Ok(addedEmployee);
         }
 
