6ff2822958a98f654203d3a5aaa0c13beb201b9b
Ihor Mizyk <imizyk09@gmail.com>
2024-02-23 15:58:08 +0200

    added cached query abstractions

---
+++ BusinessLogic/Abstractions/Messaging/ICachedQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace BusinessLogic.Abstractions.Messaging
+{
+    public interface ICachedQuery<TResponse> : IQuery<TResponse>, ICachedQuery
+    {
+
+    }
+
+    public interface ICachedQuery
+    {
+        public string CacheKey { get; }
+        public TimeSpan? Expiration { get; }
+    }
+}
---
+++ BusinessLogic/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BusinessLogic.Abstractions.Messaging
+{
+    public interface IQuery<TResponse> : IRequest<TResponse>
+    {
+    }
+}
