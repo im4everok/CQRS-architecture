8bf6e3e76ffb325b32fbf9ffa91ec05115b4ef46
Ihor Mizyk <imizyk09@gmail.com>
2024-02-23 15:58:51 +0200

    added query caching pipeline behavior

---
+++ BusinessLogic/Abstractions/Behaviours/QueryCachingPipelineBehavior.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLogic.Abstractions.Caching;
+using BusinessLogic.Abstractions.Messaging;
+
+using MediatR;
+
+namespace BusinessLogic.Abstractions.Behaviours
+{
+    public sealed class QueryCachingPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : ICachedQuery
+    {
+        private readonly ICacheService _cacheService;
+
+        public QueryCachingPipelineBehavior(ICacheService cacheService)
+        {
+            _cacheService = cacheService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var result = await _cacheService.GetOrCreateAsync(request.CacheKey,
+                _ => next(),
+                request.Expiration, 
+                cancellationToken);
+
+            return result;
+        }
+    }
+}
