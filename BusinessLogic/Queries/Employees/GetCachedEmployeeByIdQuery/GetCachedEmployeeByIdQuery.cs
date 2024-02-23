be47d6866850e052de2814ffb4474dba9e87e2eb
Ihor Mizyk <imizyk09@gmail.com>
2024-02-23 15:58:24 +0200

    added cached query and handler

---
+++ BusinessLogic/Queries/Employees/GetCachedEmployeeByIdQuery/GetCachedEmployeeByIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLogic.Abstractions.Messaging;
+using BusinessLogic.Models;
+
+namespace BusinessLogic.Queries.Employees.GetCachedEmployeeByIdQuery
+{
+    public record GetCachedEmployeeByIdQuery(int Id) : ICachedQuery<Employee>
+    {
+        public string CacheKey => $"users-by-id-{Id}";
+
+        public TimeSpan? Expiration => null;
+    }
+}
---
+++ BusinessLogic/Queries/Employees/GetCachedEmployeeByIdQuery/GetCachedEmployeeByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLogic.Models;
+
+using MediatR;
+
+namespace BusinessLogic.Queries.Employees.GetCachedEmployeeByIdQuery
+{
+    public class GetCachedEmployeeByIdQueryHandler : IRequestHandler<GetCachedEmployeeByIdQuery, Employee>
+    {
+        private readonly FakeDataStore _fakeDataStore;
+
+        public GetCachedEmployeeByIdQueryHandler(FakeDataStore fakeDataStore)
+        {
+            _fakeDataStore = fakeDataStore;
+        }
+        public async Task<Employee> Handle(GetCachedEmployeeByIdQuery request, CancellationToken cancellationToken)
+        {
+            var employee = (await _fakeDataStore.GetAllEmployees()).FirstOrDefault(e => e.Id == request.Id);
+
+            return employee;
+        }
+    }
+}
